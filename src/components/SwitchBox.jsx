5c064dfebca0850d8c50e245551d423ec5c79400
Alex Merced <alex@alexmerced.dev>
2022-05-15 08:14:07 -0400

    react 18

--- src/components/SwitchBox.jsx
+++ src/components/SwitchBox.jsx
@@ -1,22 +1,15 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Page2 from "./pages/Page2.jsx";
 
 export const SwitchBox = (props) => {
   return (
     <main>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/page">
-          <Page2 />
-        </Route>
-        <Route>
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/page" element={<Page2 />}/>
+      </Routes>
     </main>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import { Nav } from "./components/Nav.jsx";
 import { SwitchBox } from "./components/SwitchBox.jsx";
+import { createRoot } from "react-dom/client";
 
 import "./styles.scss";
 
@@ -17,4 +18,5 @@ const App = (props) => {
 };
 
 //append app to root div
-ReactDom.render(<App />, document.querySelector("#root"));
+const root = createRoot(document.querySelector("#root"));
+root.render(<App />)
